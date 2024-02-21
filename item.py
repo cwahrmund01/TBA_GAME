a64cc29282ae7f664cff7310417ea8aea1f27a7d
Cedric <cwahrmund@gmail.com>
2024-02-20 21:17:38 -0600

    Added Lantern child to Item

--- item.py
+++ item.py
@@ -4,7 +4,20 @@ class Item():
 
 
 class Weapon(Item):
-    def __init__(self, name, damage, one_time=False):
+    def __init__(self, name, damage, is_one_time_hit=False):
         super().__init__(name)
         self.damage = damage
-        self.one_time = one_time
+        self.is_one_time_hit = is_one_time_hit
+
+class Waterskin(Item):
+    def __init__(self, name="waterskin", is_full=True):
+        super().__init__(name)
+        self.is_full = is_full
+
+class Lantern(Item):
+    def __init__(self, name="lantern", rounds_of_fuel=100):
+        super().__init__(name)
+        self.rounds_of_fuel = rounds_of_fuel
+    
+    def __str__(self):
+        print(f"Your lantern will burn for {self.rounds_of_fuel} more moves.")
