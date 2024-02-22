3b119c574b12c9e3e7a0d7e0f2d00b7b738c9959
Cedric <cwahrmund@gmail.com>
2024-02-21 20:49:55 -0600

    Added methods to inventory

--- inventory.py
+++ inventory.py
@@ -5,8 +5,40 @@ class Inventory():
         self.contents = argv
 
         self.capacity = 10
-        self.has_pack = False
+    
+    def __str__(self):
+        pass
 
+    def get_number_items(self):
+        return len(self.contents)
+    
+    def inventory_is_full(self):
+        return self.get_number_items() == self.capacity
+
+    def get_item_by_name(self, item_name):
+        for i in range(self.get_number_items()):
+            if self.contents[i].name == item_name:
+                return self.contents[i]
+        
+        return None
+
+    def get_item_index_by_name(self, name):
+        for i in range(self.get_number_items()):
+            if self.contents[i].name == name:
+                return i
+        
+        return None
+
+    def remove_item(self, item):
+        index = self.get_item_index_by_name(item.name)
+        self.contents.pop(index)
+    
+    def remove_item_by_name(self, item_name):
+        index = self.get_item_index_by_name(item_name)
+        self.contents.pop(index)
+
+    def add_item(self, item):
+        self.contents.append(item)
 
 def inventory_from_json(json_inventory):
     pass
