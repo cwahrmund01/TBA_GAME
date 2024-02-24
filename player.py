23bfe38014936c31d705e28e79929e2da3ecf32f
Cedric <cwahrmund@gmail.com>
2024-02-24 13:30:14 -0600

    Removed os library
    I added the library to see why importing the file failed

--- player.py
+++ player.py
@@ -1,7 +1,6 @@
 import item
 import inventory
 import json
-import os
 
 class Player():
     def __init__(self, name, health, inventory):
@@ -20,7 +19,6 @@ class Player():
 
 
 def player_from_json(json_player):
-    print(os.getcwd())
     with open("./save_states/initial.json") as f:
         data = json.load(f)
         json_player = data["player"]
