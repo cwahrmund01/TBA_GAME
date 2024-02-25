7ee3acb35d680593763d6db33670e97f06719e4d
cwahrmund01 <cedricwahrmund@gmail.com>
2024-02-25 00:49:45 -0600

    Player import now on player.py, remove global var
    Forgot to change function for importing player info
    to not pull from file and instead use arguement passed.
    Switched from Global to local vars on main, globals
    can be finicky and it is not necessary.

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 import player
 import json
 
-PLAYER = None
-MAP = None
-NPCS = None
-
-def import_initial_game_state():
+def import_initial_game_state(player_name):
     with open("./save_states/initial.json", "r") as init_file:
         game_data = json.load(init_file)
         json_player = game_data["player"]
         json_rooms = game_data["rooms"]
         json_npcs = game_data["npcs"]
 
-        PLAYER = player.player_from_json(json_player)
+        PLAYER = player.player_from_json(json_player, name=player_name)
+        return PLAYER
 
 def main():
-    # print("Welcome to <title of game here>!")
-    # name = input("First, what is your name, adventurer? ").lower().capitalize()
-    # player_obj = player.Player(name, STARTING_INVENTORY)
-    # print(player_obj)
-
-    p1 = player.player_from_json(1)
-
-    print(p1.name)
-    print(p1.inventory)
-    p1.inventory.sort_contents()
-    print(p1.inventory)
+    print("Welcome to Cedric's Text Based Adventure Game!")
+    name = input("First, what is your name, adventurer? ").lower().title()
+    PLAYER = import_initial_game_state(name)
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -20,8 +20,6 @@ class Player():
 
 def player_from_json(json_player, name=None):
     with open("./save_states/initial.json") as f:
-        data = json.load(f)
-        json_player = data["player"]
         name = json_player["name"]
         health = json_player["health"]
         json_inventory = json_player["inventory"]
